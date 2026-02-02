680d3804c9c8aecaaa34e212f68f4a290b17ccbb
batman <vidhmarwaha21@gmail.com>
2026-02-02 21:42:23 +0530

    Update password.js

--- password/password.js
+++ password/password.js
@@ -1,3 +1,15 @@
+const SITE_PASSWORD = "PV=nRT"; 
+
+function checkPassword() {
+  const input = document.getElementById("password-input").value;
+  const error = document.getElementById("password-error");
+
+  if (input === SITE_PASSWORD) {
+    document.getElementById("password-screen").style.display = "none";
+  } else {
+    error.textContent = "Wrong password 💔";
+  }
+}
 if (localStorage.getItem("siteUnlocked") === "yes") {
   document.getElementById("password-screen").style.display = "none";
 }
